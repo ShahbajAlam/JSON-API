7c396e39ae7fcf5222828197ecbd460bd83696ec
Your Name <you@example.com>
2024-03-12 13:59:49 +0530

    modified:   components/Carousel.tsx

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -17,22 +17,18 @@ const Carousel = () => {
                 if (index === 2) return 0;
                 else return index + 1;
             });
-        }, 5000);
+        }, 3000);
 
         return () => clearInterval(id);
     }, []);
 
     return (
         <div className="relative w-[25rem] h-[25rem] flex justify-center items-center bg-red-400 rounded-2xl overflow-hidden">
-            {images.map((image, i) => (
-                <img
-                    key={i}
-                    src={image}
-                    className={`w-full h-full absolute bg-cover inset-0 translate-x-[${
-                        i === currentIndex ? 0 : 100
-                    }%] transition-all duration-700 ease-out z-[${i}]`}
-                />
-            ))}
+            <img
+                src={images[currentIndex]}
+                alt={`image-${currentIndex}`}
+                className="w-full h-full bg-cover"
+            />
         </div>
     );
 };
